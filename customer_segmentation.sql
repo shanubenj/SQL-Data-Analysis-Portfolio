084ecdd4bec93385157edd33e7623488450bae2d
shanubenj <155158595+shanubenj@users.noreply.github.com>
2026-03-08 16:34:19 +0530

    Create customer_segmentation.sql

---
+++ customer_segmentation.sql
@@ -0,0 +1,31 @@
+-- Project: Customer Segmentation Project
+-- Author: Shanu Benjamin
+-- Description: SQL analysis of customers by region or behavior
+-- Skills Demonstrated:
+-- GROUP BY
+-- Aggregation functions (COUNT)
+
+-- 1. Create Table
+CREATE TABLE Customers (
+    CustomerID INT,
+    CustomerName VARCHAR(50),
+    Country VARCHAR(50),
+    City VARCHAR(50)
+);
+
+-- 2. Insert Sample Data
+INSERT INTO Sales VALUES
+(1, 'Alice', 'Germany', 'Berlin'),
+(2, 'Bob', 'Germany','Munich'),
+(3, 'Ravi', 'India', 'Delhi');
+
+--3. Customers per country
+SELECT Country, COUNT(*) AS TotalCustomers
+FROM Customers
+GROUP BY Country
+ORDER BY TotalCustomers DESC;
+
+--4. Customers per city
+SELECT City, COUNT(*) AS Customers
+FROM Customers
+GROUP BY City;
